6c0d643b61cc3ad4307b71d3fd550126cbf090a6
rMarduk87 <shaun87@live.it>
2024-12-01 17:27:53 +0100

    Aggiornamenti grafici

--- app/src/main/java/rpt/tool/pongclock/ui/clock/ClockFragment.kt
+++ app/src/main/java/rpt/tool/pongclock/ui/clock/ClockFragment.kt
@@ -1,12 +1,18 @@
 package rpt.tool.pongclock.ui.clock
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import rpt.tool.pongclock.BaseFragment
+import rpt.tool.pongclock.R
 import rpt.tool.pongclock.databinding.ClockFragmentBinding
+import rpt.tool.pongclock.utils.AppUtils
+import rpt.tool.pongclock.utils.extensions.toColor
+import rpt.tool.pongclock.utils.manager.SharedPreferencesManager
 import rpt.tool.pongclock.utils.navigation.safeNavController
 import rpt.tool.pongclock.utils.navigation.safeNavigate
 import rpt.tool.pongclock.utils.view.PongTimeView.PongThread
+import java.util.Calendar
 
 
 @Suppress("DEPRECATION")
@@ -21,6 +27,8 @@ class ClockFragment : BaseFragment<ClockFragmentBinding>(ClockFragmentBinding::i
 
         pongThread = binding.pongview.thread
 
+        binding.settings.setColorFilter(getColor())
+
         binding.settings.setOnClickListener {
             goToSettingsFragment()
         }
@@ -30,5 +38,19 @@ class ClockFragment : BaseFragment<ClockFragmentBinding>(ClockFragmentBinding::i
     private fun goToSettingsFragment() {
         safeNavController?.safeNavigate(ClockFragmentDirections.
         actionClockFragmentToSettingsFragment())
+    }
+
+    private fun getColor(): Int {
+        var color = Color.WHITE
+        if(SharedPreferencesManager.mode == 1 && SharedPreferencesManager.season == 0){
+            color = requireContext().getColor(R.color.matrix)
+        }
+        else if((SharedPreferencesManager.mode == 1 || SharedPreferencesManager.mode ==0)
+            && SharedPreferencesManager.season == 1){
+            val calendar = Calendar.getInstance()
+            val currentDayOfYear = calendar[Calendar.DAY_OF_YEAR]
+            color = requireContext().getColor(AppUtils.getSeason(currentDayOfYear).toColor())
+        }
+        return color
     }
 }
